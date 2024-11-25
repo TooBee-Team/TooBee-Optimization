02cca8693e828eb2c3ce61e818defa2a9ed78c30
Fungus <jkfungus@yandex.com>
2024-11-25 18:06:06 +0800

    Add warden cache

--- src/main/kotlin/top/toobee/optimization/cache/BeCached.kt
+++
@@ -1,7 +0,0 @@
-package top.toobee.optimization.cache
-
-interface BeCached {
-    fun `toobee$addCache`(cache: AttachedCache<*>?)
-    fun `toobee$getCache`(): AttachedCache<*>?
-    fun `toobee$removeCache`()
-}

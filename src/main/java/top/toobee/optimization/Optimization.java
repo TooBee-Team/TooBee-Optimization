0ebc767a02ab68aa1b11d1a2618fa1d3a029ec2c
Fungus <jkfungus@yandex.com>
2025-04-11 21:57:04 +0800

    Remove kotlin dep && update to 1.21.5

---
+++ src/main/java/top/toobee/optimization/Optimization.java
@@ -0,0 +1,25 @@
+package top.toobee.optimization;
+
+import net.caffeinemc.mods.lithium.common.entity.pushable.PushableEntityClassGroup;
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+import net.minecraft.entity.mob.WardenEntity;
+
+import top.toobee.optimization.cache.PiglinCache;
+import top.toobee.optimization.cache.WardenCache;
+import top.toobee.optimization.accessor.EntityClassGroupAccessor;
+
+public final class Optimization implements DedicatedServerModInitializer {
+    public static void startServerTick() {
+        WardenCache.CACHES.tick();
+        PiglinCache.CACHES.tick();
+    }
+
+    @Override
+    public void onInitializeServer() {
+        ((EntityClassGroupAccessor) PushableEntityClassGroup.MAYBE_PUSHABLE)
+                .getClass2GroupContains().addTo(WardenEntity.class, (byte) 1);
+        ((EntityClassGroupAccessor) PushableEntityClassGroup.CACHABLE_UNPUSHABILITY)
+                .getClass2GroupContains().addTo(WardenEntity.class, (byte) 1);
+    }
+}
---
+++ src/main/java/top/toobee/optimization/cache/AttachedCache.java
@@ -0,0 +1,63 @@
+package top.toobee.optimization.cache;
+
+import net.minecraft.world.World;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+
+public interface AttachedCache<T> {
+    /**
+     * For identification. Originally it is ServerWorld, but it's always inconvenient.
+     */
+    World getWorld();
+
+    /**
+     * Used to check whether to drop this cache.
+     * @see #tick()
+     */
+    AtomicInteger getReferencedCounter();
+
+    /**
+     * Ensure the thread safety, not so useful though.
+     */
+    Lock getLock();
+
+    /**
+     * It always not used directly by external class, just for a compulsive requirement to implement.
+     * @see #getLastUpdateTick()
+     */
+    long getLastUpdateTick();
+    void setLastUpdateTick(long l);
+
+    /**
+     * Target judges whether using or updating cache depending on this
+     */
+    boolean getHasUpdatedThisTick();
+    void setHasUpdatedThisTick(boolean b);
+
+    /**
+     * Check whether the target satisfy certain condition.
+     */
+    boolean failCondition(T t);
+
+    /**
+     * When the cache is dropped, do some clear tasks.
+     */
+    void truncate();
+
+    /**
+     * Run every end of tick to reset some variables.
+     */
+    default void tick() {
+        final var time = getWorld().getTime();
+        if (getLastUpdateTick() != time) {
+            if (getHasUpdatedThisTick()) {
+                setHasUpdatedThisTick(false);
+                setLastUpdateTick(time);
+            } else if ((time - getLastUpdateTick()) >> 8 != 0L) {
+                truncate();
+            }
+            if (getReferencedCounter().get() >> 4 == 0)
+                truncate();
+        }
+    }
+}
